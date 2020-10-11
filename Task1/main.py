fbe2de56eb40a451c4d175ce5a34ee3c1982d1f4
Karol Podlewski <podlewski@hotmail.com>
2020-10-11 12:21:24 +0200

    fix: Properly ploting for decades, fixed formating for all states

--- Task1/main.py
+++ Task1/main.py
@@ -21,8 +21,8 @@ disease = args.disease
 disease_ns = disease.replace(' ', '-')
 vaccine_year = vaccines_years[disease]
 
-if not os.path.exists(f'imgs/{disease_ns}'):
-    os.makedirs(f'imgs/{disease_ns}')
+if not os.path.exists(f'imgs/{disease_ns}/states'):
+    os.makedirs(f'imgs/{disease_ns}/states')
 
 ### 1
 df = pd.read_csv('us_contagious_diseases.csv', header=0, index_col=0)
@@ -34,16 +34,17 @@ df = df[df['disease'] == disease]
 
 for state in args.states:
     state_ns = state.replace(' ', '_')
-    if not os.path.exists(f'imgs/{disease_ns}/{state_ns}'):
-        os.makedirs(f'imgs/{disease_ns}/{state_ns}')
-
     state_df = df.drop(df[df['state'] != state].index)
     state_df['count square transform'] = np.sqrt(state_df['count'])
+
+    count_max = max(state_df['count']) 
+    sqrt_count_max = max(state_df['count square transform'])
 
     plot = sns.relplot(x=state_df['year'], y=state_df['count'], kind='line', marker='o')
     plt.axvline(vaccine_year, color='r')
     plt.annotate(f'{vaccine_year} - {disease} vaccine\nintroduction',
-                xy=(vaccine_year, 60000), xytext=(vaccine_year + 8, 70000),
+                xy=(vaccine_year, count_max * 3 / 4),
+                xytext=(vaccine_year + 8, count_max * 3 / 4 + count_max / 10),
                 arrowprops=dict(facecolor='black', shrink=0.05, headwidth=10, width=3))
     plot.fig.set_size_inches(10, 6)
     plot.set_ylabels(f'{disease} disease rate')
@@ -51,14 +52,15 @@ for state in args.states:
     plt.tight_layout()
     if (args.show_plot):
         plt.show()
-    plot.savefig(f'imgs/{disease_ns}/{state_ns}/{disease_ns}_disease_rate_in_{state_ns}.png')
+    plot.savefig(f'imgs/{disease_ns}/states/{disease_ns}_disease_rate_in_{state_ns}.png')
     plt.cla()
 
     plot = sns.relplot(x=state_df['year'], y=state_df['count square transform'],
                        kind='line', marker='o')
     plt.axvline(vaccine_year, color='r')
     plt.annotate(f'{vaccine_year} - {disease} vaccine\nintroduction',
-                xy=(vaccine_year, 200), xytext=(vaccine_year + 8, 250),
+                xy=(vaccine_year, sqrt_count_max * 3 / 4),
+                xytext=(vaccine_year + 8, sqrt_count_max * 3 / 4 + sqrt_count_max / 10),
                 arrowprops=dict(facecolor='black', shrink=0.05, headwidth=10, width=3))
     plot.fig.set_size_inches(10, 6)
     plot.set_ylabels(f'{disease} disease square rate')
@@ -66,18 +68,35 @@ for state in args.states:
     plt.tight_layout()
     if (args.show_plot):
         plt.show()
-    plot.savefig(f'imgs/{disease_ns}/{state_ns}/{disease_ns}_disease_rate_in_{state_ns}_sqrt_transform.png')
-    plt.cla()
+    plot.savefig(f'imgs/{disease_ns}/states/{disease_ns}_disease_rate_in_{state_ns}_sqrt_transform.png')
+    plt.close()
 
-plt.close()
+count_max = 0
+sqrt_count_max = 0
 
 for decade in args.decades:
-    deacde_df = df[df['year'] == decade]
+    deacde_df = df[(df['year'] >= decade) & (df['year'] < decade+10)]
+    deacde_df = deacde_df[['state', 'count']].groupby('state').sum()
+    deacde_df['count square transform'] = np.sqrt(deacde_df['count'])
+
+    decade_max = max(deacde_df['count'])
+    if (decade_max > count_max):
+        count_max = decade_max
+
+    decade_sqrt_max = max(deacde_df['count square transform'])
+    if (decade_sqrt_max > sqrt_count_max):
+        sqrt_count_max = decade_sqrt_max      
+
+count_max = (int(count_max / 25000) + 1) * 25000 
+sqrt_count_max = (int(sqrt_count_max / 100) + 1) * 100 
+
+for decade in args.decades:
+    deacde_df = df[(df['year'] >= decade) & (df['year'] < decade+10)]
     deacde_df = deacde_df[['state', 'count']].groupby('state').sum()
     deacde_df['count square transform'] = np.sqrt(deacde_df['count'])
 
     plot = sns.barplot(x=deacde_df['count'], y=deacde_df.index)
-    plt.xlim(0, 50000)
+    plt.xlim(0, count_max)
     if decade < 1960:
         plt.gca().get_yticklabels()[1].set_color("red")
         #plt.gca().get_yticklabels()[11].set_color("red")
@@ -93,7 +112,7 @@ for decade in args.decades:
     plt.cla()
 
     plot = sns.barplot(x=deacde_df['count square transform'], y=deacde_df.index)
-    plt.xlim(0, 250)
+    plt.xlim(0, sqrt_count_max)
     if decade < 1960:
         plt.gca().get_yticklabels()[1].set_color("red")
         #plt.gca().get_yticklabels()[11].set_color("red")
@@ -107,7 +126,6 @@ for decade in args.decades:
     plt.cla()
 
 plt.close()
-
 
 # box 
 plot = sns.boxplot(x=df['year'], y=df['state'], data=df['count'])
