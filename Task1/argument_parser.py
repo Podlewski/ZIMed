4dfc33d37a25877f5caa409ab57eb697a4aeb4ff
Karol Podlewski <podlewski@hotmail.com>
2020-10-11 11:49:54 +0200

    feat: Added option to work with given diseases, states and decades

--- Task1/argument_parser.py
+++ Task1/argument_parser.py
@@ -17,12 +17,46 @@ class ArgumentParser:
                         '\n  Karol Podlewski\t234106'
                         '\n  Andrzej Sasinowski\t???') 
 
-        self.parser.add_argument('-s', '--show', dest='show_plot', 
+        self.parser.add_argument('--show', dest='show_plot', 
                                  action='store_const', const=True, default=False,
                                  help='Show plots during program run')
 
-        self.args = self.parser.parse_args()
+        self.parser.add_argument('-d', metavar='DISEASE', dest='disease', 
+                                 type=str, default='Measles',
+                                 choices=['Hepatitis A', 'Measles', 'Mumps',
+                                          'Pertussis', 'Polio', 'Rubella', 'Smallpox'],
+                                 help='Specify disease to work with: Hepatitis A, '
+                                      'Measles, Mumps, Pertussis, Polio, '
+                                      'Rubella or Smallpox (default Measles)')
 
+        self.parser.add_argument('-s', metavar='STATE', dest='states', 
+                                 type=str, nargs='+', default=['California'],
+                                 choices=['Alabama', 'Alaska', 'Arizona', 'Arkansas',
+                                          'California', 'Colorado', 'Connecticut',
+                                          'Delaware','District Of Columbia', 'Florida',
+                                          'Georgia', 'Hawaii', 'Idaho', 'Illinois',
+                                          'Indiana', 'Iowa', 'Kansas', 'Kentucky',
+                                          'Louisiana', 'Maine', 'Maryland', 'Massachusetts',
+                                          'Michigan', 'Minnesota', 'Mississippi', 'Missouri',
+                                          'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+                                          'New Jersey', 'New Mexico', 'New York',
+                                          'North Carolina', 'North Dakota', 'Ohio',
+                                          'Oklahoma', 'Oregon', 'Pennsylvania',
+                                          'Rhode Island', 'South Carolina', 'South Dakota',
+                                          'Tennessee', 'Texas', 'Utah', 'Vermont',
+                                          'Virginia', 'Washington', 'West Virginia',
+                                          'Wisconsin', 'Wyoming'],
+                                 help='Specify states to work with '
+                                      '(default California)')
+
+        self.parser.add_argument('-y', metavar='DECADE', dest='decades', 
+                                 type=int, nargs='+', default=[1950, 1960, 1970],
+                                 choices=[1930, 1940, 1950, 1960, 1970, 1980],
+                                 help='Specify decades to work with: 1930, '
+                                      '1940, 1950, 1960, 1970, 1980 '
+                                      '(default 1950, 1960 & 1970)')
+
+        self.args = self.parser.parse_args()
 
     def get_arguments(self):
         return self.args
--- Task1/main.py
+++ Task1/main.py
@@ -1,151 +1,121 @@
-import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import pandas as pd
+import seaborn as sns
 from argument_parser import ArgumentParser
 
 
 sns.set_theme()
 args = ArgumentParser().get_arguments()
-df = pd.read_csv('us_contagious_diseases.csv', header=0, index_col=0)
+
+vaccines_years = {'Hepatitis A': 1950,
+                   'Measles': 1963,
+                   'Mumps': 1950,
+                   'Pertussis': 1950,
+                   'Polio': 1950,
+                   'Rubella': 1950,
+                   'Smallpox': 1950}
+
+disease = args.disease
+disease_ns = disease.replace(' ', '-')
+vaccine_year = vaccines_years[disease]
+
+if not os.path.exists(f'imgs/{disease_ns}'):
+    os.makedirs(f'imgs/{disease_ns}')
 
 ### 1
-df.drop(df[(df['state'] == 'Alaska') |
-           (df['state'] == 'Hawaii') |
-           (df['disease'] != 'Measles') |
-           (df['population'] <= 100000)].index, inplace=True)
-
-### 2
-californiaDF = df.drop(df[df['state'] != 'California'].index)
-
-plot = sns.relplot(x=californiaDF['year'], y=californiaDF['count'], kind='line', marker='o')
-plt.axvline(1963, color='r')
-
-plt.annotate('1963 - measles vaccine\nintroduction', xy=(1963, 60000), xytext=(1963 + 8, 70000),
-             arrowprops=dict(facecolor='black', shrink=0.05, headwidth=10, width=3))
-plot.fig.set_size_inches(10, 6)
-plot.set_xlabels('year')
-plot.set_ylabels('measles disease rate')
-plt.title('Measles disease rate per year for California', fontsize=16)
-plt.tight_layout()
-if (args.show_plot):
-    plt.show()
-plot.savefig('imgs/measles_disease_rate_in_California.png')
-plt.close()
-
-### 3
 df = pd.read_csv('us_contagious_diseases.csv', header=0, index_col=0)
-df = df[df['disease'] == 'Measles']
+# df.drop(df[(df['state'] == 'Alaska') |
+#            (df['state'] == 'Hawaii') |
+#            (df['disease'] != 'Measles') |
+#            (df['population'] <= 100000)].index, inplace=True)
+df = df[df['disease'] == disease]
 
-hist_1950 = df[df['year'] == 1950]
-hist_1950 = hist_1950[['state', 'count']].groupby('state').sum()
-hist_1950['count square transform'] = np.sqrt(hist_1950['count'])
+for state in args.states:
+    state_ns = state.replace(' ', '_')
+    if not os.path.exists(f'imgs/{disease_ns}/{state_ns}'):
+        os.makedirs(f'imgs/{disease_ns}/{state_ns}')
 
-hist_1960 = df[df['year'] == 1960]
-hist_1960 = hist_1960[['state', 'count']].groupby('state').sum()
-hist_1960['count square transform'] = np.sqrt(hist_1960['count'])
+    state_df = df.drop(df[df['state'] != state].index)
+    state_df['count square transform'] = np.sqrt(state_df['count'])
 
-hist_1970 = df[df['year'] == 1970]
-hist_1970 = hist_1970[['state', 'count']].groupby('state').sum()
-hist_1970['count square transform'] = np.sqrt(hist_1970['count'])
+    plot = sns.relplot(x=state_df['year'], y=state_df['count'], kind='line', marker='o')
+    plt.axvline(vaccine_year, color='r')
+    plt.annotate(f'{vaccine_year} - {disease} vaccine\nintroduction',
+                xy=(vaccine_year, 60000), xytext=(vaccine_year + 8, 70000),
+                arrowprops=dict(facecolor='black', shrink=0.05, headwidth=10, width=3))
+    plot.fig.set_size_inches(10, 6)
+    plot.set_ylabels(f'{disease} disease rate')
+    plt.title(f'{disease} disease rate per year for {state}', fontsize=16)
+    plt.tight_layout()
+    if (args.show_plot):
+        plt.show()
+    plot.savefig(f'imgs/{disease_ns}/{state_ns}/{disease_ns}_disease_rate_in_{state_ns}.png')
+    plt.cla()
 
-# plot 1950
-plot_histogram_1950 = sns.barplot(x=hist_1950['count'], y=hist_1950.index)
-plt.xlim(0, 50000)
-plt.gca().get_yticklabels()[1].set_color("red")
-#plt.gca().get_yticklabels()[11].set_color("red")
-plt.figtext(0.01, 0.01, "*red states did not exist at that time", ha="left", fontsize=12, color='red')
-fig = plot_histogram_1950.get_figure()
-fig.set_size_inches(6, 12)
-plt.title('Measles disease rate in 1950', fontsize=16)
-plt.tight_layout()
-if (args.show_plot):
-    plt.show()
-fig.savefig("imgs/measles_disease_rate_in_1950.png")
-plt.clf()
+    plot = sns.relplot(x=state_df['year'], y=state_df['count square transform'],
+                       kind='line', marker='o')
+    plt.axvline(vaccine_year, color='r')
+    plt.annotate(f'{vaccine_year} - {disease} vaccine\nintroduction',
+                xy=(vaccine_year, 200), xytext=(vaccine_year + 8, 250),
+                arrowprops=dict(facecolor='black', shrink=0.05, headwidth=10, width=3))
+    plot.fig.set_size_inches(10, 6)
+    plot.set_ylabels(f'{disease} disease square rate')
+    plt.title(f'{disease} disease square rate per year for {state}', fontsize=16)
+    plt.tight_layout()
+    if (args.show_plot):
+        plt.show()
+    plot.savefig(f'imgs/{disease_ns}/{state_ns}/{disease_ns}_disease_rate_in_{state_ns}_sqrt_transform.png')
+    plt.cla()
 
-# plot 1960
-plot_histogram_1960 = sns.barplot(x=hist_1960['count'], y=hist_1960.index)
-plt.xlim(0, 50000)
-fig = plot_histogram_1960.get_figure()
-fig.set_size_inches(6, 12)
-plt.title('Measles disease rate in 1960', fontsize=16)
-plt.tight_layout()
-if (args.show_plot):
-    plt.show()
-fig.savefig("imgs/measles_disease_rate_in_1960.png")
-plt.clf()
-
-# plot 1970
-plot_histogram_1970 = sns.barplot(x=hist_1970['count'], y=hist_1970.index)
-plt.xlim(0, 50000)
-fig = plot_histogram_1970.get_figure()
-fig.set_size_inches(6, 12)
-plt.title('Measles disease rate in 1970', fontsize=16)
-plt.tight_layout()
-if (args.show_plot):
-    plt.show()
-fig.savefig("imgs/measles_disease_rate_in_1970.png")
-plt.clf()
-
-# plot 1950 square root transformation
-plot_histogram_1950 = sns.barplot(x=hist_1950['count square transform'], y=hist_1950.index)
-plt.xlim(0, 250)
-plt.gca().get_yticklabels()[1].set_color("red")
-#plt.gca().get_yticklabels()[11].set_color("red")
-plt.figtext(0.01, 0.01, "*red states did not exist at that time", ha="left", fontsize=12, color='red')
-fig = plot_histogram_1950.get_figure()
-fig.set_size_inches(6, 12)
-plot.set_xlabels('count')
-plt.title('Measles disease rate in 1950', fontsize=16)
-plt.tight_layout()
-if (args.show_plot):
-    plt.show()
-fig.savefig("imgs/measles_disease_rate_in_1950_sqrt_transform.png")
-plt.clf()
-
-# plot 1960 square root transformation
-plot_histogram_1960 = sns.barplot(x=hist_1960['count square transform'], y=hist_1960.index)
-plt.xlim(0, 250)
-fig = plot_histogram_1960.get_figure()
-fig.set_size_inches(6, 12)
-plot.set_xlabels('count')
-plt.title('Measles disease rate in 1960', fontsize=16)
-plt.tight_layout()
-if (args.show_plot):
-    plt.show()
-fig.savefig("imgs/measles_disease_rate_in_1960_sqrt_transform.png")
-plt.clf()
-
-# plot 1970 square root transformation
-plot_histogram_1970 = sns.barplot(x=hist_1970['count square transform'], y=hist_1970.index)
-plt.xlim(0, 250)
-
-fig = plot_histogram_1970.get_figure()
-fig.set_size_inches(6, 12)
-plot.set_xlabels('count')
-plt.title('Measles disease rate in 1970', fontsize=16)
-plt.tight_layout()
-plt.tight_layout()
-if (args.show_plot):
-    plt.show()
-fig.savefig("imgs/measles_disease_rate_in_1970_sqrt_transform.png")
 plt.close()
 
-### 4
-californiaDF['count square transform'] = np.sqrt(californiaDF['count'])
+for decade in args.decades:
+    deacde_df = df[df['year'] == decade]
+    deacde_df = deacde_df[['state', 'count']].groupby('state').sum()
+    deacde_df['count square transform'] = np.sqrt(deacde_df['count'])
 
-plot = sns.relplot(x=californiaDF['year'], y=californiaDF['count square transform'], kind='line', marker='o')
-plt.axvline(1963, color='r')
+    plot = sns.barplot(x=deacde_df['count'], y=deacde_df.index)
+    plt.xlim(0, 50000)
+    if decade < 1960:
+        plt.gca().get_yticklabels()[1].set_color("red")
+        #plt.gca().get_yticklabels()[11].set_color("red")
+        plt.figtext(0.01, 0.01, "*red states did not exist at that time",
+                    ha="left", fontsize=12, color='red')
+    fig = plot.get_figure()
+    fig.set_size_inches(6, 12)
+    plt.title(f'{disease} disease rate in {decade}', fontsize=16)
+    plt.tight_layout()
+    if (args.show_plot):
+        plt.show()
+    fig.savefig(f'imgs/{disease_ns}/{disease_ns}_disease_rate_in_{decade}.png')
+    plt.cla()
 
-plt.annotate('1963 - measles vaccine\nintroduction', xy=(1963, 225), xytext=(1963 + 8, 275),
-             arrowprops=dict(facecolor='black', shrink=0.05, headwidth=10, width=3))
-plot.fig.set_size_inches(10, 6)
-plot.set_xlabels('year')
-plot.set_ylabels('measles disease square rate')
-plt.title('Measles disease rate per year for California', fontsize=16)
+    plot = sns.barplot(x=deacde_df['count square transform'], y=deacde_df.index)
+    plt.xlim(0, 250)
+    if decade < 1960:
+        plt.gca().get_yticklabels()[1].set_color("red")
+        #plt.gca().get_yticklabels()[11].set_color("red")
+        plt.figtext(0.01, 0.01, "*red states did not exist at that time",
+                    ha="left", fontsize=12, color='red')
+    plot.set(xlabel='square count')
+    plt.title(f'{disease} disease rate in {decade}', fontsize=16)
+    if (args.show_plot):
+        plt.show()
+    fig.savefig(f'imgs/{disease_ns}/{disease_ns}_disease_rate_in_{decade}_sqrt_transform.png')
+    plt.cla()
+
+plt.close()
+
+
+# box 
+plot = sns.boxplot(x=df['year'], y=df['state'], data=df['count'])
+fig = plot.get_figure()
+fig.set_size_inches(6, 12)
+plt.title(f'Distribution of {disease} rates', fontsize=16)
 plt.tight_layout()
 if (args.show_plot):
     plt.show()
-plot.savefig('imgs/measles_disease_rate_in_California_sqrt_transform.png')
-plt.clf()
+fig.savefig(f'imgs/{disease_ns}/Distribution_of_{disease_ns}_rates.png')
+plt.close()
